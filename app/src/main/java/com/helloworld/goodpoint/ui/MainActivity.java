afb057024f1d181bc5486e8bba5b97a29b857d02
dinamishahed <Dina01090780888>
2020-12-09 23:14:40 +0200

    test

--- app/src/main/java/com/helloworld/goodpoint/ui/MainActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/MainActivity.java
@@ -2,7 +2,10 @@ package com.helloworld.goodpoint.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.helloworld.goodpoint.R;
 
@@ -12,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.btn);
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
--- app/src/main/java/com/helloworld/goodpoint/ui/SignupActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/SignupActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.helloworld.goodpoint.R;
+
 public class SignupActivity extends AppCompatActivity {
 
     @Override
