a4e0e8b9732f709104f18c132c735334fc11a0ab
Ahmed_plus_plus <ahmed8576498@gmail.com>
2020-12-10 18:06:22 +0200

    divide the code of MapFragment

--- app/src/main/java/com/helloworld/goodpoint/ui/MapFragment.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/MapFragment.java
@@ -31,13 +31,11 @@ public class MapFragment extends Fragment {
     SupportMapFragment mapFragment;
     FusedLocationProviderClient client;
     Location curLocation;
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    String []permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
         /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
          * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
@@ -46,12 +44,13 @@ public class MapFragment extends Fragment {
         public void onMapReady(GoogleMap googleMap) {
             LatLng curLatLng = new LatLng(curLocation.getLatitude(), curLocation.getLongitude());
             LatLng neighbourLatLng = new LatLng(curLocation.getLatitude()+0.1, curLocation.getLongitude()+0.1);
+
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLatLng,15));
+
             googleMap.addMarker(new MarkerOptions().position(curLatLng).title("My location"));
             googleMap.addMarker(new MarkerOptions().position(neighbourLatLng));
         }
     };
-    String []permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     @Nullable
     @Override
@@ -64,19 +63,17 @@ public class MapFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        client = LocationServices.getFusedLocationProviderClient(getContext());
-        if (ActivityCompat.checkSelfPermission(
-                getContext(), permissions[0]) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                    getContext(), permissions[1]) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
+
+        init();
+        runGoogleMap();
+
+    }
+
+    private void runGoogleMap() {
+        if (ActivityCompat.checkSelfPermission(getContext(), permissions[0]) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), permissions[1]) != PackageManager.PERMISSION_GRANTED) {
+
             ActivityCompat.requestPermissions(getActivity(), permissions, MAP_CODE);
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -89,6 +86,12 @@ public class MapFragment extends Fragment {
             }
         });
     }
+
+    private void init() {
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        client = LocationServices.getFusedLocationProviderClient(getContext());
+    }
+
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
         switch(requestCode){
             case MAP_CODE:
