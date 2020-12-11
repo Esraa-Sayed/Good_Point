fd2c6250f9f65222f9efe3140e7753cfe6ed63cf
abdelrhman-mamdouh <63111171+abdelrhman-mamdouh@users.noreply.github.com>
2020-12-12 01:28:59 +0200

    IntroSlider

--- app/src/main/java/com/helloworld/goodpoint/ui/HomeActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/HomeActivity.java
@@ -1,12 +1,14 @@
 package com.helloworld.goodpoint.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.helloworld.goodpoint.R;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
