47bbfd6ec23c72a49f51590d261475ecce58d3e0
dinamishahed <Dina01090780888>
2020-12-16 21:59:43 +0200

    Revert ".."
    This reverts commit 815e0693e4b129ce26610574d28e90aee7b913c9, reversing
    changes made to f094512fa929fa748e5bc61871dfd7f58f5c9d41.

--- app/src/main/java/com/helloworld/goodpoint/ui/MainActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/MainActivity.java
@@ -12,13 +12,13 @@ import android.widget.Toast;
 import com.helloworld.goodpoint.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button test;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test = findViewById(R.id.btn);
-        test.setOnClickListener(new View.OnClickListener() {
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent in =new Intent(MainActivity.this, SigninActivity.class);
