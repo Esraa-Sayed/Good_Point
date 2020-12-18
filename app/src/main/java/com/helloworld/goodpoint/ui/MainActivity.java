f4d6cd8cdab2db48cb4a63b2acbde56dbb286777
Ahmed_plus_plus <ahmed8576498@gmail.com>
2020-12-18 20:22:58 +0200

    some updates

--- app/src/main/java/com/helloworld/goodpoint/ui/MainActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/MainActivity.java
@@ -12,18 +12,12 @@ import android.widget.Toast;
 import com.helloworld.goodpoint.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in =new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(in);
-            }
-        });
+        startActivity(new Intent(this,SigninActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/helloworld/goodpoint/ui/SigninActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/SigninActivity.java
@@ -2,17 +2,20 @@ package com.helloworld.goodpoint.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.helloworld.goodpoint.R;
 
 import java.util.Calendar;
 
-public class SigninActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText Email,Pass;
     private TextView ForgetPass;
     private CheckBox RememberMe;
@@ -28,7 +31,31 @@ public class SigninActivity extends AppCompatActivity {
         Email = findViewById(R.id.email);
         Pass =findViewById(R.id.pass);
         ForgetPass = findViewById(R.id.forgetPass);
-        Sigin =findViewById(R.id.sigin);
+        Sigin =findViewById(R.id.signin);
         CreateNewAccount = findViewById(R.id.NewAccount);
+
+        Sigin.setOnClickListener(this);
+        CreateNewAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.signin:
+                if(validAccount()) {
+                    startActivity(new Intent(this, HomeActivity.class));
+                    finish();
+                }else
+                    Toast.makeText(this, "Invalid account", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.NewAccount:
+                    startActivity(new Intent(this, SignupActivity.class));
+                    break;
+        }
+    }
+
+    private boolean validAccount() {
+        //check validation
+        return true;
     }
 }
