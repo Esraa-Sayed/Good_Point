0081d007d4cd281649e23e38c5002c56ef5724a6
Ahmed_plus_plus <ahmed8576498@gmail.com>
2021-01-29 06:18:49 +0200

    back

--- backend/user_account/views.py
+++ backend/user_account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
 # Create your views here.
+from .models import User
+
+
