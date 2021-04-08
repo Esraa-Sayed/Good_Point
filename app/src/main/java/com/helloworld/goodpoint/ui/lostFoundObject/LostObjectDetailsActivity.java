1ae48c412c704538b9cba03e395dedebba8d7366
Esraa-Sayed <53404081+Esraa-Sayed@users.noreply.github.com>
2021-04-09 00:33:46 +0200

    face detection in lost activity but not have finished yet

--- app/src/main/java/com/helloworld/goodpoint/ui/lostFoundObject/LostObjectDetailsActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/lostFoundObject/LostObjectDetailsActivity.java
@@ -7,6 +7,7 @@ import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -30,20 +31,32 @@ import com.helloworld.goodpoint.R;
 import com.helloworld.goodpoint.ui.prepareList;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.List;
 
 public class LostObjectDetailsActivity extends AppCompatActivity implements View.OnClickListener,objectDataType{
     private TextView DateT ;
+    private File caseFile ;
+    private CascadeClassifier faceDetector;
     private Button Person , Object,Match;
     private Fragment PersonF,ObjectF;
     private FragmentManager FM ;
@@ -161,7 +174,18 @@ public class LostObjectDetailsActivity extends AppCompatActivity implements View
     Mat sampledImg;
     private boolean CheckMatchPerson()
     {
-        OpenCVLoader.initDebug();
+        if(!OpenCVLoader.initDebug())
+        {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,this,baseCallback);
+        }
+        else
+        {
+            try {
+                baseCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         EditText PersonName =  PersonF.getView().findViewById(R.id.PersonName);
         ImageView IV = PersonF.getView().findViewById(R.id.imageView);
         PName = PersonName.getText().toString();
@@ -201,7 +225,7 @@ public class LostObjectDetailsActivity extends AppCompatActivity implements View
                int mobile_height = size.y;
                sampledImg = new Mat();
               double downSampleRatio = calculateSubSimpleSize(rgbImg,mobile_width,mobile_height);
-              Imgproc.resize(rgbImg,sampledImg,new Size(),downSampleRatio,downSampleRatio,Imgproc.INTER_AREA);
+//              Imgproc.resize(rgbImg,sampledImg,new Size(),downSampleRatio,downSampleRatio,Imgproc.INTER_AREA);
                try {
                    ExifInterface exifInterface = new ExifInterface(path);
                    int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
@@ -219,8 +243,17 @@ public class LostObjectDetailsActivity extends AppCompatActivity implements View
                } catch (IOException e) {
                    e.printStackTrace();
                }
-               Bitmap bitmap = Bitmap.createBitmap(sampledImg.cols(),sampledImg.rows(),Bitmap.Config.RGB_565);
-               Utils.matToBitmap(sampledImg,bitmap);
+               MatOfRect faceDetections = new MatOfRect();
+               faceDetector.detectMultiScale(rgbImg,faceDetections);
+               for(Rect rect:faceDetections.toArray())
+               {
+
+                      Imgproc.rectangle(rgbImg,new org.opencv.core.Point(rect.x,rect.y),
+                              new org.opencv.core.Point(rect.x+rect.width,rect.y+rect.height),
+                              new Scalar(0,255,0));
+               }
+               Bitmap bitmap = Bitmap.createBitmap(rgbImg.cols(),rgbImg.rows(),Bitmap.Config.RGB_565);
+               Utils.matToBitmap(rgbImg,bitmap);
                IV.setVisibility(View.VISIBLE);
                IV.setImageBitmap(bitmap);
                Log.e("Path", "CheckMatchPerson: "+path);
@@ -228,7 +261,41 @@ public class LostObjectDetailsActivity extends AppCompatActivity implements View
         }
         return true;
     }
+private BaseLoaderCallback baseCallback = new BaseLoaderCallback(this) {
+    @Override
+    public void onManagerConnected(int status) throws IOException {
+        switch (status) {
+            case LoaderCallbackInterface.SUCCESS: {
+                InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+                File CasCadeDire = getDir("cascade", Context.MODE_PRIVATE);
+                 caseFile = new File(CasCadeDire,"haarcascade_frontalface_alt2.xml");
+                FileOutputStream fos = new FileOutputStream(caseFile);
+                byte[] buffer = new byte[4096];
+                int byteRead;
+                while((byteRead = is.read(buffer))!=-1)
+                {
+                    fos.write(buffer,0,byteRead);
+                }
+                is.close();
+                fos.close();
+                faceDetector = new CascadeClassifier(caseFile.getAbsolutePath());
+                if(faceDetector.empty())
+                {
+                    faceDetector = null;
+                }
+                else
+                {
+                    CasCadeDire.delete();
+                }
+            }
+            break;
+            default:
+                super.onManagerConnected(status);
+                break;
+        }
 
+    }
+};
     private double calculateSubSimpleSize(Mat rgbImg, int mobile_width, int mobile_height) {
         final int width = rgbImg.width();
         final int height = rgbImg.height();
