becb09ec5f587b7e4755ea434de1a8f8e041d1a1
abdelrhman-mamdouh <abdelrhman99m@gmail.com>
2021-05-01 15:04:55 +0200

    بس كده

--- app/src/main/java/com/helloworld/goodpoint/ui/select_multiple_faces/Selection.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/select_multiple_faces/Selection.java
@@ -3,10 +3,12 @@ package com.helloworld.goodpoint.ui.select_multiple_faces;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,13 +16,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.helloworld.goodpoint.R;
 import com.helloworld.goodpoint.ui.GlobalVar;
-import com.helloworld.goodpoint.ui.HomeActivity;
+import com.helloworld.goodpoint.ui.test;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Selection extends AppCompatActivity {
+public class Selection extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private static final String TAG = "Selection";
     //vars
@@ -28,91 +30,96 @@ public class Selection extends AppCompatActivity {
     private ArrayList<String> mImageUrls = new ArrayList<>();
     RadioButton rb;
     Button Done_btn;
+    ImageView imageView;
+    SubItemListAdapter recyclerViewAdapter;
+    RecyclerView rvItem;
+    List<List<Bitmap>> faces;
+    List<Bitmap> mfaces;
 
+    boolean click = false;
+    int k = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multiple_faces_selection);
-
-        getImages();
-        RecyclerView rvItem = findViewById(R.id.recycler_view_items);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(com.helloworld.goodpoint.ui.select_multiple_faces.Selection.this);
-        ItemListAdapter itemAdapter = new ItemListAdapter(buildItemList(), this);
-        rvItem.setAdapter(itemAdapter);
-        rvItem.setLayoutManager(layoutManager);
+        rvItem = findViewById(R.id.recycler_view_sub_items);
+        imageView = findViewById(R.id.ItemImage);
+        rb = (RadioButton) findViewById(R.id.radioButton);
+        faces = GlobalVar.allFaces;
+        mfaces = GlobalVar.ImgThatHaveMoreThanOneFace;
+        List<Bitmap> mfaces = GlobalVar.ImgThatHaveMoreThanOneFace;
+        click = false;
+        createCard(k);
         Done_btn = (Button) findViewById(R.id.Done_btn);
-        Done_btn.setOnClickListener(new View.OnClickListener() {
+        if (k == mfaces.size() - 1) {
+            Done_btn.setText("Done");
+        }
+            Done_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FancyToast.makeText(Selection.this, "Thank you", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
-                startActivity(new Intent(Selection.this, HomeActivity.class));
-                finish();
+                if (click) {
+                    SubItemList list = GlobalVar.slist.get(GlobalVar.position);
+                    GlobalVar.FinialFacesThatWillGoToDataBase.add(list.getSubItemImage());
+                    if (k == mfaces.size() - 1) {
+                        startActivity(new Intent(Selection.this, test.class));
+                        finish();
+                    } else {
+                        k++;
+                        createCard(k);
+                        click = false;
+                        if (k == mfaces.size() - 1) {
+                            Done_btn.setText("Done");
+                        }
+
+                    }
+                } else
+                    FancyToast.makeText(Selection.this, "Specify the face of the missing person", FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+
+
+//
             }
         });
 
 
     }
 
-    //Toast.makeText(Selection.this,
-//"size="+faces.get(i).size(),
-//Toast.LENGTH_LONG).show();
-    private List<com.helloworld.goodpoint.ui.select_multiple_faces.ItemList> buildItemList() {
-        List<com.helloworld.goodpoint.ui.select_multiple_faces.ItemList> itemList = new ArrayList<>();
-        List<List<Bitmap>> faces = GlobalVar.allFaces;
-        List<Bitmap> mfaces = GlobalVar.ImgThatHaveMoreThanOneFace;
-        for (int i = 0; i < mfaces.size(); i++) {
-            com.helloworld.goodpoint.ui.select_multiple_faces.ItemList item = new com.helloworld.goodpoint.ui.select_multiple_faces.ItemList(mfaces.get(i), buildSubItemList(faces.get(i)));
-            itemList.add(item);
-        }
-        return itemList;
+    private void createCard(int i) {
+        imageView.setImageBitmap(mfaces.get(i));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                rvItem.getContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+        );
+        recyclerViewAdapter = new SubItemListAdapter(buildSubItemList(faces.get(i)), this);
+        recyclerViewAdapter.setOnItemClickListener(this);
+        rvItem.setAdapter(recyclerViewAdapter);
+        rvItem.setLayoutManager(layoutManager);
+
     }
+
 
     private List<com.helloworld.goodpoint.ui.select_multiple_faces.SubItemList> buildSubItemList(List<Bitmap> faces) {
         List<com.helloworld.goodpoint.ui.select_multiple_faces.SubItemList> subItemList = new ArrayList<>();
         for (int i = 0; i < faces.size(); i++) {
-            com.helloworld.goodpoint.ui.select_multiple_faces.SubItemList subItem = new com.helloworld.goodpoint.ui.select_multiple_faces.SubItemList(faces.get(i),i);
+            com.helloworld.goodpoint.ui.select_multiple_faces.SubItemList subItem = new com.helloworld.goodpoint.ui.select_multiple_faces.SubItemList(faces.get(i), i);
             subItemList.add(subItem);
         }
-        // addRadioButtons(10);
+        GlobalVar.slist = subItemList;
 
         return subItemList;
     }
 
-    private void getImages() {
-        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        SubItemList list = GlobalVar.slist.get(position);
+        click = true;
+        int i = list.getPos();
+        i += 1;
+        GlobalVar.position=position;
 
-        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        mNames.add("Havasu Falls");
-
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
-
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
-
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
-
-
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Mahahual");
-
-        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-        mNames.add("Frozen Lake");
-
-
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("White Sands Desert");
-
-        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-        mNames.add("Austrailia");
-
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Washington");
-
-
+        Toast.makeText(this, "Face number (" + i
+                + ") is selected", Toast.LENGTH_SHORT).show();
     }
-
-
 }
