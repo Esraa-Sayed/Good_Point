a50f12f9bd0cf4ea7a17f479cbb5af08078bf058
dinamishahed <Dina01090780888>
2021-05-03 15:11:05 +0200

    ..

--- backend/find_losts/models.py
+++ backend/find_losts/models.py
@@ -5,9 +5,8 @@ from notification.models import Notification
 
 # Create your models here.
 
-
 class LostObject(models.Model):
-    date = models.DateTimeField()
+    date = models.DateField()
     city = models.CharField(max_length=35)
     user_id = models.ForeignKey(User, related_name='lost', on_delete=models.CASCADE, db_column='user_id')
     is_matched = models.BooleanField(default=False)
