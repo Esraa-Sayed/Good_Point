ce6a3902f22b26c8f1e44a8ae2473cf86920d09c
abdelrhman-mamdouh <abdelrhman99m@gmail.com>
2021-09-25 00:48:17 +0200

    >>>

--- app/src/main/java/com/helloworld/goodpoint/ui/lostFoundObject/LostObjectDetailsActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/lostFoundObject/LostObjectDetailsActivity.java
@@ -35,8 +35,8 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 import com.google.gson.JsonObject;
 import com.helloworld.goodpoint.R;
-import com.helloworld.goodpoint.pojo.FoundPerson;
 import com.helloworld.goodpoint.pojo.LostItem;
+import com.helloworld.goodpoint.pojo.LostPerson;
 import com.helloworld.goodpoint.pojo.User;
 import com.helloworld.goodpoint.retrofit.ApiClient;
 import com.helloworld.goodpoint.retrofit.ApiInterface;
@@ -281,9 +281,9 @@ public class LostObjectDetailsActivity extends AppCompatActivity implements View
         protected void onPostExecute(Void a) {
             super.onPostExecute(a);
             if (GlobalVar.allFaces.size() > 0) {
-                FoundPerson.getFoundPerson().setName(PName);
-                FoundPerson.getFoundPerson().setDate(DateT.getText().toString().trim());
-                FoundPerson.getFoundPerson().setCity(City);
+                LostPerson.getLostPerson().setName(PName);
+                LostPerson.getLostPerson().setDate(DateT.getText().toString().trim());
+                LostPerson.getLostPerson().setCity(City);
                 startActivity(new Intent(LostObjectDetailsActivity.this, Alert.class));
                 GlobalVar.flag=2;
                 finish();
