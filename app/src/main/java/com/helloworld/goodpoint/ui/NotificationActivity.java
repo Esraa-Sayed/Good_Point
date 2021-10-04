5e42170ba6a42775f2ddaafb208b34b2db09ccd7
Esraa-Sayed <53404081+Esraa-Sayed@users.noreply.github.com>
2021-10-04 02:39:20 +0200

    fix error

--- app/src/main/java/com/helloworld/goodpoint/ui/NotificationActivity.java
+++ app/src/main/java/com/helloworld/goodpoint/ui/NotificationActivity.java
@@ -142,7 +142,7 @@ public class NotificationActivity extends AppCompatActivity {
                             Choose.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    if (ActivityCompat.checkSelfPermission(getApplicationContext(),  Manifest.permissionManifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                                    if (ActivityCompat.checkSelfPermission(getApplicationContext(),  Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                                             && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                                         ActivityCompat.requestPermissions(NotificationActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 11);
                                     }
